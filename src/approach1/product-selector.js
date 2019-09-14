5e9870a5212ebe31af3731f593abfc633e8c3cc9
kelon zhou <kelonzhou@gmail.com>
2019-09-14 12:32:09 -0400

    Fixed react warning

--- src/approach1/product-selector.js
+++ src/approach1/product-selector.js
@@ -3,21 +3,42 @@ import React from 'react';
 const ProductSelector = ({onChange}) => (
   <div>
     <select onChange={e => onChange({id: e.target.value})}>
-      <option value="NormalProduct">NormalProduct</option>
-      <option value="ProductWithClearanceAvailable">
+      <option key="NormalProduct" value="NormalProduct">
+        NormalProduct
+      </option>
+      <option
+        key="ProductWithClearanceAvailable"
+        value="ProductWithClearanceAvailable"
+      >
         ProductWithClearanceAvailable
       </option>
-      <option value="ProductWithSets">ProductWithSets</option>
-      <option value="FlooringProduct">FlooringProduct</option>
-      <option value="WallpaperInDE">WallpaperInDE</option>
-      <option value="RugProduct">RugProduct</option>
-      <option value="ClearanceProduct">ClearanceProduct</option>
-      <option value="OnSaleProduct">OnSaleProduct</option>
-      <option value="RestrictedProduct">RestrictedProduct</option>
+      <option key="ProductWithSets" value="ProductWithSets">
+        ProductWithSets
+      </option>
+      <option key="FlooringProduct" value="FlooringProduct">
+        FlooringProduct
+      </option>
+      <option key="WallpaperInDE" value="WallpaperInDE">
+        WallpaperInDE
+      </option>
+      <option key="RugProduct" value="RugProduct">
+        RugProduct
+      </option>
+      <option key="ClearanceProduct" value="ClearanceProduct">
+        ClearanceProduct
+      </option>
+      <option key="OnSaleProduct" value="OnSaleProduct">
+        OnSaleProduct
+      </option>
+      <option key="RestrictedProduct" value="RestrictedProduct">
+        RestrictedProduct
+      </option>
     </select>
     <select onChange={e => onChange({quantity: parseInt(e.target.value)})}>
       {[1, 2, 3, 4, 5].map(quantity => (
-        <option value={quantity}>{quantity}</option>
+        <option key={quantity} value={quantity}>
+          {quantity}
+        </option>
       ))}
     </select>
   </div>
