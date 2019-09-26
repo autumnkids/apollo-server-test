20edc651bd280313c8cf77007f70e57a518db783
kelon zhou <kelonzhou@gmail.com>
2019-09-25 21:50:16 -0400

    Update folder name ./approach3-improved to ./approach3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from '@apollo/react-hooks';
 // import ProductBlock from './approach1/ProductBlock';
 // import ProductBlock from './approach2.1/product-block';
-import ProductBlock from './approach2.2/product-block';
-// import ProductBlock from './current-approach/product-block';
-// import ProductBlock from './approach3-improved/product-block';
+// import ProductBlock from './approach2.2/product-block';
+import ProductBlock from './current-approach/product-block';
+// import ProductBlock from './approach3/product-block';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
