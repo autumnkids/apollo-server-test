ba7f9871f37ebc55ca08f52b75751767ac313f36
kelon zhou <kelonzhou@gmail.com>
2019-09-25 21:29:29 -0400

    Update ./approach3 to ./current-approach

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from '@apollo/react-hooks';
 // import ProductBlock from './approach1.1/product-block';
 // import ProductBlock from './approach1.2/product-block';
-import ProductBlock from './approach2/ProductBlock';
-// import ProductBlock from './approach3/product-block';
+// import ProductBlock from './approach2/ProductBlock';
+import ProductBlock from './current-approach/product-block';
 // import ProductBlock from './approach3-improved/product-block';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'http://localhost:4000'
 });
 
 const App = () => (
